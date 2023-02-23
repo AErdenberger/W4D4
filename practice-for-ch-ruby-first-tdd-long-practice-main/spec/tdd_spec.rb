b52dc10ca0445283d7abdf96634224b80f6865f5
Ahmed <ahmed96razi39@gmail.com>
2023-02-23 14:51:52 -0800

    Ahmed's first commit

--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -3,16 +3,28 @@ require "tdd"
 
 describe "Array" do
     describe "my_uniq" do
+        subject(:arr_test) {[1, 2, 1, 3, 3]}
         it "is called on an array" do
-            expect(self.is_a?(Array)).to eq(true)
+            expect(arr_test.is_a?(Array)).to eq(true)
         end
 
         it "returns an array of unique elements" do
-            expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3])
+            expect(arr_test.my_uniq).to eq([1, 2, 3])
         end
 
     end
-end
 
-#test to see if we're passing into an array
-#self.is_a?(Array)
+    describe "two_sum" do 
+        subject(:sum_test) {[-1, 0, 2, -2, 1]}
+        it "returns a 2-D array" do 
+            expect(sum_test.two_sum[0].is_a?(Array)).to eq(true)
+        end
+
+        it "indices in resulting subarrays, map to elements in original array add to 0" do 
+            el_1 = sum_test.two_sum[0][0]
+            el_2 = sum_test.two_sum[0][1]
+            expect(sum_test[el_1] + sum_test[el_2] == 0).to eq(true)
+        end
+        #Array.new(2) {Array.new(2)}
+    end
+end
